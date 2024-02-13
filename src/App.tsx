32653611e2e73aa6b058b1079096fe36bb8ae62a
reneumeh <reneumeh22@gmail.com>
2024-02-13 16:46:37 +0900

    +korean page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import Main from "./pages/Main";
 import Blog from "./pages/Blog";
+import MainKOR from "./pages/Main_KOR";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/blog" element={<Blog />} />
+          <Route path="/kor" element={<MainKOR />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
