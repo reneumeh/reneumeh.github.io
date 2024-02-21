5d3a5f8880768923854e9eef04bd1d23943a2c6d
reneumeh <reneumeh22@gmail.com>
2024-02-21 22:35:30 +0900

    jnoj

--- src/pages/MainENG.tsx
+++ src/pages/MainENG.tsx
@@ -854,6 +854,7 @@ const PortfolioWrapper = styled.div<{ hoveredElement: string }>`
             .portfolio-name {
                 font-size: 22px;
                 width: 160px;
+                top: -2.5vw;
                 left: 42vw;
             }
         }
