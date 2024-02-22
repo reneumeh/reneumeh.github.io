0db1402d0520ad9583b8e379183653f6dc9422e1
reneumeh <reneumeh22@gmail.com>
2024-02-22 10:28:03 +0900

    hashbrowser

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import MainENG from "./pages/MainENG";
 import Blog from "./pages/Blog";
@@ -7,14 +7,14 @@ import MainKOR from "./pages/MainKOR";
 function App() {
   return (
     <>
-      <BrowserRouter basename={"/"}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<MainENG />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/kor" element={<MainKOR />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
