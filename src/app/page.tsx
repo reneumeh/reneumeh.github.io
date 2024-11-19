3f27b6f8f65d0ce37c3aaf0afcd4972acc3df862
reneumeh <87000105+reneumeh@users.noreply.github.com>
2024-11-19 11:32:56 +0900

    browser router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 "use client"
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
 import MainENG from "./pages/MainENG";
 import { BlogENG } from "./pages/BlogENG";
 import { BlogKOR } from "./pages/BlogKOR";
 import MainKOR from "./pages/MainKOR";
-import dynamic from 'next/dynamic';
-
-const ClientSideHashRouter = dynamic(() => import('react-router-dom').then(mod => mod.HashRouter), { ssr: false });
 
 function Home() {
   return (
     <>
-      <ClientSideHashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainENG />} />
           <Route path="/blog/*" element={<BlogENG />} />
@@ -20,7 +17,7 @@ function Home() {
           <Route path="/kor" element={<MainKOR />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </ClientSideHashRouter>
+      </BrowserRouter>
     </>
   );
 }
